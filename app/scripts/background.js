d489457befd4256e300bdcf701a4f308e5091a6a
unknown <90906166@UWSE0699545FF8C.AD.UWS.EDU.AU>
2014-04-14 12:45:23 +1000

    Testing alternate approaches from Chrome 34

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,52 @@
 'use strict';
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-	if (changeInfo.url === 'chrome://newtab/') {
-		
-		chrome.tabs.update(tabId, {
-			'url': 'chrome://apps/',
-			'active': true
-		});
-	}
-});
+function NewTabAppsPage() {
+
+   if (!(this instanceof NewTabAppsPage))
+      return new NewTabAppsPage();
+
+   var me = this;
+
+   this.a = 'a';
+
+   /**
+    * Load the Chrome apps page.
+    */
+   this.loadAppsPage = function loadAppsPage(tabId) {
+      console.log('Loading apps page');
+      chrome.tabs.update(tabId, {
+         'url': 'chrome://apps/',
+         'active': true,
+         'highlighted': true
+      }, function(tab) {
+         // chrome.tabs.reload(newTabId);
+      });
+   }
+}
+
+/**
+ * Add tab updated handler.
+ */
+NewTabAppsPage.prototype.AddHandler = function addHandler() {
+   chrome.tabs.onUpdated.addListener(function(newTabId, changeInfo) {
+      if (changeInfo.url === 'chrome://newtab/') {
+
+         // Testing whether waiting for it to be loaded will make a difference with the address bar.
+         if (changeInfo.status === 'loading') { //'complete') {
+            loadAppsPage(newTabId);
+         } else {
+            console.log('Still loading. Tab ID: %s. URL: %s. Status: %s',
+               newTabId,
+               changeInfo.url,
+               changeInfo.status);
+
+            chrome.tabs.onUpdated.removeListener(function() {
+               addHandler();
+            });
+         }
+      }
+   });
+}
+
+var ntap = new NewTabAppsPage();
+ntap.AddHandler();
--- readme.md
+++ readme.md
@@ -7,11 +7,15 @@
 # TODO
 1. Make sure it works with multiple windows
 2. Incorporate analytics
-3. Add options page:
-	- Set focus on address bar
+3. Fix packaging to include icon
+4. Improve icons
 
 
 # CHANGELOG
+
+v
+* Restricted extension to Chrome 33+
+* Fixed issue in Chrome 34 which caused address bar text to be unselected.
 
 v0.0.28
 * When opening new tab, address bar remains focused so you can commence typing immediately
